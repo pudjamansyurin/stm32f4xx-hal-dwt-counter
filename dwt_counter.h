03c88b9e629bd45ed43ab4442cd2b566c21c36a9
Puja Kusuma <pujakusumae@hotmail.com>
2022-02-24 06:01:45 +0700

    Added get time feature

--- dwt_counter.h
+++ dwt_counter.h
@@ -11,14 +11,14 @@
 #include "stm32f4xx_hal.h"
 
 /* Public function declarations & definitions */
-__STATIC_INLINE void DWT_Init(void)
+__STATIC_INLINE void DWT_Start(void)
 {
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
   DWT->CYCCNT = 0;
   DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 }
 
-__STATIC_INLINE void DWT_DeInit(void)
+__STATIC_INLINE void DWT_Stop(void)
 {
   DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;
   DWT->CYCCNT = 0;
@@ -27,13 +27,33 @@ __STATIC_INLINE void DWT_DeInit(void)
 
 __STATIC_INLINE void DWT_Restart(void)
 {
-  DWT_DeInit();
-  DWT_Init();
+  DWT_Stop();
+  DWT_Start();
 }
 
 __STATIC_INLINE uint32_t DWT_GetCounter(void)
 {
   return (DWT->CYCCNT);
+}
+
+__STATIC_INLINE float DWT_GetTime(void)
+{
+  return ((float) DWT->CYCCNT / HAL_RCC_GetHCLKFreq());
+}
+
+__STATIC_INLINE float DWT_GetTime_ms(void)
+{
+  return (DWT_GetTime() * 1000);
+}
+
+__STATIC_INLINE float DWT_GetTime_us(void)
+{
+  return (DWT_GetTime() * 1000000);
+}
+
+__STATIC_INLINE float DWT_GetTime_ns(void)
+{
+  return (DWT_GetTime() * 1000000000);
 }
 
 #endif /* STM32F4XX_DWT_COUNTER_DWT_COUNTER_H_ */
